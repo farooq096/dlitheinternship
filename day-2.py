5a7ec11c2a1bc3a44e06c77d17d2cb9efc14e2bd
farooq096 <mohdomerfarooq79@gmail.com>
2025-02-19 16:51:03 +0530

    pyhton code

---
+++ day-2.py
@@ -0,0 +1,215 @@
+a=""" 
+life
+    is
+        amazing
+            if
+                you
+                    are
+                        patient
+
+
+
+print(a)
+
+a="farooq"
+print(a)
+
+
+
+a="farooq "good""
+print(a)
+
+
+
+a='farooq "good"'
+print(a)
+
+
+
+a="farooq"
+print(a[1])
+
+
+a="life is beautiful","ture","false"
+print(a[2])
+
+
+a="life is beautiful"
+print(len(a))
+
+
+
+
+
+
+a="work hard fail again but fail better"
+print("again" in a)
+print("pass" in a)
+
+
+
+a="work hard fail again but fail better"
+if "fail" in a:
+    print("true")
+
+
+a="work hard fail again but fail better"
+if "fail" in a:
+    print("true")
+else:
+    print("false")
+
+
+
+a="work hard fail again but fail better"
+if "fail" not in a:
+    print("true")
+else:
+    print("false")
+
+
+a="work hard"
+print(a[:5])
+
+a="work hard"
+print(a[5:])
+
+
+m="medium"
+print(m.upper())
+
+
+m="MEDIUM"
+print(m.lower())
+
+
+
+a="hello world"
+print(a.replace(" ","o"))
+
+
+
+a="hello world"
+b=a.split(",")
+print(b)
+
+
+
+
+a="life"
+b="is"
+c="good"
+print(a+b+c)
+
+
+
+
+
+age=24
+a=f"my name is farooq ,my age is {age}"
+print(a)
+
+
+
+
+feild="AI&DS"
+b="RCB"
+c ="ABD IS AN ALIEN"
+a=f"my name is farooq ,my feild is {feild} and ,my fvrt ipl team is {b} and {c}"
+print(a)
+
+
+
+
+
+print(10>12)
+print(2345<54321)
+print(1013>12)
+print(234512<54321)
+print(1013==12)
+print(234512!=54321)
+
+
+a=int(input("enter the age of a::::"))
+b=int(input("enter the age of b::::"))
+res=a+b
+print(res)
+
+
+a=int(input("enter the age of a::::"))
+b=int(input("enter the age of b::::"))
+print(a+b)
+print(a-b)
+print(a/b)
+print(a%b)
+print(a==b)
+
+"""
+
+
+a="hell,o,world"
+b=a.split(",")
+print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ prog-2.py
@@ -0,0 +1,2 @@
+a="farooq"
+print(a)
